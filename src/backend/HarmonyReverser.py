eec358a3f7479edd12ff85512dd0ef79fc68408d
Ivan Romanenko <giovanni.romanenko@gmail.com>
2024-06-23 18:18:39 +0200

    Changed error messages

--- src/backend/HarmonyReverser.py
+++ src/backend/HarmonyReverser.py
@@ -15,7 +15,7 @@ class HarmonyReverser:
     def __init__(self, filename):
         self._errorMessage = None
         if not self._fileIsMIDI(filename):
-            self._errorMessage = "Only MIDI files supported"
+            self._errorMessage = "Only MIDI files supported (.mid)"
             return
         self._midiFile = self._readMidiFile(filename)
 
@@ -36,5 +36,5 @@ class HarmonyReverser:
             return pretty_midi.PrettyMIDI(filename)
         except (ValueError, OSError, EOFError) as error:
             ErrorHandle.handleError(error, "Tried to read MIDI file, but it's probably not MIDI file")
-            self._errorMessage = "Failed to read file"
+            self._errorMessage = "Failed to process MIDI file"
             return None
